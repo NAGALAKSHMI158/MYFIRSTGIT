97f5c86068b2171187a7750485e2b48d61afb745
NAGALAKSHMI158 <nagasubramanian158@gmail.com>
2020-10-17 17:40:53 +0530

    MY FIRST COMMIT

---
+++ palindrome.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class palindrome
+{
+	public static void main (String[] args){
+
+
+    Scanner a = new Scanner(System.in);
+    System.out.print("enter any number: ");
+    int num = a.nextInt();
+    int reversednumber=0; int remainder,originalnumber;
+    originalnumber = num; 
+    while(num!=0){
+    	remainder = num%10;
+    	reversednumber=reversednumber*10+remainder;
+    	num=num/10;
+    }
+if(originalnumber==reversednumber){
+	System.out.println("it's a palindrome");
+}
+else{
+	System.out.println("it's not a palindrome");
+}
+}
+}
